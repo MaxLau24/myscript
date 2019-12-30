9f34f16bb1cab1d3d20ebe351337adf1a37fed90
None <29383402+MaxLau24@users.noreply.github.com>
2019-12-30 15:04:43 +0800

    Update iQIYI_GetCookie.js

--- iQIYI_GetCookie.js
+++ iQIYI_GetCookie.js
@@ -1,44 +1,48 @@
 /*
-iQIYI Checkin Get Cookie. by NobyDa
+iQIYI Checkin Get Cookie.
 
-After launching iQIYI app. You can disable this script when a success notification pops up.
-Note that the following config is only a local script configuration, please put this script into Quantumult X/Script
+Update 2019.12.28 21:30
+
+About the author:
+If reproduced, indicate the source
+Telegram channel: @NobyDa
+Telegram bots: @NobyDa_bot
+
+Description :
+When iQiyi app is opened, click "My", If notification gets cookie success, you can use the check in script. because script will automatically judgment whether the cookie is updated, so you dont need to disable it manually.
+
+script will be performed every day at 9 am. You can modify the execution time.
+Note that the following config is only a local script configuration, please put both scripts into Quantumult X/Script, and the cookie script only works for iQIYI apps in china apple store
 
 [rewrite_local]
-https:\/\/passport\.iqiyi\.com\/apis\/user\/info\.action.*authcookie url script-response-body iQIYI_GetCookie.js
+# Get iQIYI cookie
+https:\/\/passport\.iqiyi\.com\/apis\/user\/info\.action.*authcookie url script-response-body iQIYI_GetCookie_QX.js
 
-MITM = passport.iqiyi.com
+# MITM = passport.iqiyi.com
+
+[task_local]
+0 9 * * * iQIYI_DailyBonus_QX.js
+
 */
 
 var regex = /authcookie=([A-Za-z0-9]+)/;
 var iQIYI = regex.exec($request.url)[1];
 
 if ($prefs.valueForKey("CookieQY") != undefined) {
-if ($prefs.valueForKey("CookieQY") != iQIYI) {
-var cookie = $prefs.setValueForKey(iQIYI, "CookieQY");
-  if (!cookie){
-    $notify("更新爱奇艺签到Cookie失败‼️", "", "")
+  if ($prefs.valueForKey("CookieQY") != iQIYI) {
+    var cookie = $prefs.setValueForKey(iQIYI, "CookieQY");
+    if (!cookie) {
+      $notify("更新爱奇艺签到Cookie失败‼️", "", "")
     } else {
       $notify("更新爱奇艺签到Cookie成功 🎉", "", "")
     }
-}
+  }
 } else {
-var cookie = $prefs.setValueForKey(iQIYI, "CookieQY");
-  if (!cookie){
+  var cookie = $prefs.setValueForKey(iQIYI, "CookieQY");
+  if (!cookie) {
     $notify("首次写入爱奇艺Cookie失败‼️", "", "")
-    } else {
-      $notify("首次写入爱奇艺Cookie成功 🎉", "", "")
-    }
+  } else {
+    $notify("首次写入爱奇艺Cookie成功 🎉", "", "")
+  }
 }
 $done({})
-/*
-var cookie = $prefs.setValueForKey(iQIYI, "CookieQY");
-
-  if (!cookie){
-    $notify("写入爱奇艺Cookie失败‼️‼️", "", "请重试")
-    } else {
-      $notify("写入爱奇艺Cookie成功🎉", "", "您可以手动禁用此脚本")
-      console.log("爱奇艺Cookie : \n" + $prefs.valueForKey("CookieQY"))
-    }
-  $done({})
-  */
